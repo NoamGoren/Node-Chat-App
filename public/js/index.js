bcbc1072670542c926a0f61338e130ed674bd017
InigoMontoyaa <noam.goren@gmail.com>
2018-01-03 21:13:20 +0200

    Added css

--- public/js/index.js
+++ public/js/index.js
@@ -29,24 +29,32 @@ locationButton.on('click',function() {
 if(!navigator.geolocation){
     return alert('Geolocation not supported by your browser');
 }
+
+locationButton.attr('disabled','disabled').text('Sending location..');
+
+
 navigator.geolocation.getCurrentPosition(function(postion) {
+    locationButton.removeAttr('disabled').text('Send location');
 socket.emit('createLocationMessage',{
     latitude: postion.coords.latitude,
     longitude: postion.coords.longitude
 });
 },function(){
+    locationButton.removeAttr('disabled').text('Send location');
+    
     alert('Unable to fetch location.');
 })
-
 });
 
 jQuery('#message-form').on('submit',function(e){
 e.preventDefault();
 
+var messageTextbox=jQuery('[name=message]');
+
 socket.emit('createMessage',{
     from:'User',
-    text:jQuery('[name=message]').val()
+    text:messageTextbox.val()
 },function() {
-
+    messageTextbox.val('')
 });
 });
--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ socket.broadcast.emit('newMessage',generateMessage('Admin','New user joind!'));
 socket.on('createMessage',(message,callback)=>{
   console.log('create Message',message);
   io.emit('newMessage',generateMessage(message.from,message.text));
-callback('This is from the server');
+callback();
 })
 
 socket.on('createLocationMessage',(coords)=>{
